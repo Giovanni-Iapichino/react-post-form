ba0fcea4c1a4dad7e2fddb60c6eab549887241b7
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-29 14:33:22 +0200

    add-usestate-and-handlechange-function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,20 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Main() {
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
   return;
 }
